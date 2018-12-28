f260d5dffb13530fc18bb790e51152b6040683a2
PowderHan <728031989@qq.com>
2018-12-28 21:26:25 +0800

    2018-12-28

--- 2018/try.c
+++ 2018/try.c
@@ -3,8 +3,6 @@ using namespace std;
 
 int main() {
     int a, b;
-    freopen("in.txt","r",stdin);
-    freopen("out.txt","w",stdout);
     cin>>a>>b;
     cout<<a+b<<endl;
     return 0;
--- 2018/try.cpp
+++ 2018/try.cpp
@@ -4,17 +4,7 @@ using namespace std;
 int h, s;
 int f[25];
 
-int dfs(int cur) {
-    if(cur < 0) return 0;
-    if(f[cur])  return f[cur];
-    for(int i = 1; i <= s; i++)
-        f[cur] += dfs(cur - i);
-    return f[cur];
-}
-
 int main() {
-    cin>>h>>s;
-    f[0] = 1;
-    printf("%d\n",dfs(h));
+    cout<<5<<" "<<11<<endl;
 	return 0;
 }
