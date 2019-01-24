bff2250a3e24e035a7d1671353bf7b37b85c4c21
PowderHan <728031989@qq.com>
2019-01-24 20:14:50 +0800

    PowderHan

--- 2019/Jan/first.cpp
+++ 2019/Jan/first.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    int x;
-    int a[10005];
     cin >> n;
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    cin >> x;
-    for (int i = n; i >= x; i--)
-        a[i + 1] = a[i];
-    a[x] = a[n + 1];
-    for (int i = 1; i <= n; i++)
-        cout << a[i] << ' ';
-    cout << endl;
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            int ans = min(j, n - j + 1);
+            ans = min(ans, i);
+            ans = min(ans, n - i + 1);
+            cout << ans << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
