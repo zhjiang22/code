03b4c161080c43d80a1e2eca4a0e5265f3ea7e3d
PowderHan <728031989@qq.com>
2019-02-13 21:52:41 +0800

    PowderHan

---
+++ 2019/Feb/leetcode33.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int found(vector<int> nums, int L, int R, int keys) {
+        int l = L, r = R;
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (keys <= nums[mid])  r = mid;
+            else    l = mid + 1;
+        }
+        return nums[l] == keys ? l : -1;
+    }
+    int binary_found(int l, int r, int val, vector<int> nums) {
+        if (l > r)  return -1;
+        if (l == r)
+            return nums[l] == val ? l : -1;
+        int mid = (l + r) >> 1;
+        if (nums[mid] >= nums[l]) {
+            if (val <= nums[mid] && val >= nums[l])
+                return found(nums, l , mid, val);
+            else
+                return binary_found(mid + 1, r, val, nums);
+        }
+        else {
+            if (val >= nums[mid] && val <= nums[r])
+                return found(nums, mid, r, val);
+            else
+                return binary_found(l, mid - 1, val, nums);
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        int l = nums.size();
+        if (l == 0) return -1;
+        return binary_found(0, l - 1, target, nums);
+    }
+};
